618e15384b0b365fe18edcdc7daf6b2a68da5906
harshrana14-fi <jatoliyaharsh8@gmail.com>
2025-07-12 23:13:12 +0530

    Update IntroLoader.tsx

--- app/components/IntroLoader.tsx
+++ app/components/IntroLoader.tsx
@@ -28,7 +28,7 @@ export default function IntroLoader() {
     >
       {/* Logo */}
       <Image
-        src="/nyaylogo.png"
+        src="/nyagriklogo.png"
         alt="Nyay Logo"
         width={100}
         height={100}
